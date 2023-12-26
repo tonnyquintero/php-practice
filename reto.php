49b83952ec819fe50ebfa5ccbe01230957873f6d
tonny187 <tonyoanthony@gmail.com>
2023-12-26 12:34:26 -0500

    adding a challenge

--- reto.php
+++ reto.php
@@ -1,16 +1,36 @@
 <?php
 
+
 function getChildrenAge($age) {
-    if ($age <5) {
-        echo "El niño tendra su juguete en la parte inferior de la bodega";
-    } elseif ($age = 5 || 6 || 7) {
-        echo "El niño tendra su juguete en la parte media de la bodega";
-    } elseif ($age >8) {
-        echo "El niño tendra su juguete en la parte alta de la bodega";
+    if ($age > 0 && $age < 5) {
+
+        //$bodega ='inferior';
+        return "la parte inferior de la bodega.";
+
+    } elseif ($age >= 5 && $age <= 7) {
+
+       // $bodega = 'media';
+       return "la parte media de la bodega.";
+
+    } elseif ($age > 7) {
+
+       // $bodega = 'alta';
+       return "la parte alta de la bodega.";
+
+
     } else {
-        echo "El niño tendra su juguete en la bodega de al lado";
-    }
+        $bodega = 'Indefinida';
+    }   
     
 }
 
-getChildrenAge(100);
+$edadEstudiante = 6;
+echo "El estudiante con $edadEstudiante años, tendrá sus juguetes en " . getChildrenAge($edadEstudiante);
+
+function arbolito($pisos) {
+    for ($i=0; $i < $pisos ; $i++) { 
+        echo str_repeat('*', $i ) . "\n";
+    }
+}
+
+arbolito(10);
